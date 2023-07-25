1efc3e236aeaeb78a27b47283978cd7c1ad0d834
jim <jag.arnold.go@gmail.com>
2023-07-25 14:58:56 -0700

    add more validation for downpayment

--- app/calculators/mortgage_calculator.rb
+++ app/calculators/mortgage_calculator.rb
@@ -1,15 +1,17 @@
-class MortgageCalculator 
+class MortgageCalculator
   include ActiveModel::Model
 
-  PAYMENT_FREQUENCY = %w(weekly bi-weekly monthly)
+  PAYMENT_FREQUENCY = %w[weekly bi-weekly monthly]
 
   attr_accessor :property_price, :down_payment, :annual_interest_rate, :amortization_period, :payment_frequency
 
-  validates :property_price, :down_payment, :annual_interest_rate, :amortization_period, :payment_frequency, presence: true
-  validates :property_price, :down_payment, :annual_interest_rate, :amortization_period, numericality: { greater_than: 0 }
+  validates :property_price, :down_payment, :annual_interest_rate, :amortization_period, :payment_frequency,
+            presence: true
+  validates :property_price, :down_payment, :annual_interest_rate, :amortization_period,
+            numericality: { greater_than: 0 }
   validates :payment_frequency, inclusion: { in: PAYMENT_FREQUENCY }
   validates :amortization_period, inclusion: { in: (5..30).step(5).to_a }
-  validate :down_payment_price_check
+  validate :down_payment_price_check, if: -> { down_payment.present? && property_price.present? }
 
   def calculate
     calculate_mortgage unless valid?
@@ -17,18 +19,15 @@ class MortgageCalculator
 
   private
 
-  def calculate_mortgage
-
-  end
+  def calculate_mortgage; end
 
   def down_payment_price_check
-    if down_payment > property_price
-      errors.add(:down_payment, "can't be greater than property price")
-    end
+    messages = []
+    messages << "can't be greater than or equal to the property price" if down_payment >= property_price
+    messages << "can't be less than 5% of property price" if down_payment < (property_price * 0.05)
 
-    if down_payment < (property_price * 0.05)
-      errors.add(:down_payment, "can't be less than 5% of property price")
-    end
+    return if messages.empty?
+
+    errors.add(:down_payment, messages.join(', '))
   end
-
 end
--- spec/calculators/mortgage_calculator_spec.rb
+++ spec/calculators/mortgage_calculator_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe MortgageCalculator, type: :model do
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:property_price) }
     it { should validate_presence_of(:down_payment) }
     it { should validate_presence_of(:annual_interest_rate) }
@@ -17,16 +17,25 @@ RSpec.describe MortgageCalculator, type: :model do
     it { should validate_inclusion_of(:amortization_period).in_array((5..30).step(5).to_a) }
   end
 
-  describe "custom validations" do
-    let(:calculator) { MortgageCalculator.new(property_price: 100000, down_payment: 5000, annual_interest_rate: 5, amortization_period: 25, payment_frequency: "monthly") }
-
-    it "should add error if down payment is greater than property price" do
-      calculator.down_payment = 100001
-      calculator.valid?
-      expect(calculator.errors[:down_payment]).to include("can't be greater than property price")
+  describe 'custom validations' do
+    let(:calculator) do
+      MortgageCalculator.new(property_price: 100_000, down_payment: 5000, annual_interest_rate: 5, amortization_period: 25,
+                             payment_frequency: 'monthly')
     end
 
-    it "should add error if down payment is less than 5% of property price" do
+    it 'should add error if down payment is greater than property price' do
+      calculator.down_payment = 100_001
+      calculator.valid?
+      expect(calculator.errors[:down_payment]).to include("can't be greater than or equal to the property price")
+    end
+
+    it 'should add error if down payment is equal to the property price' do
+      calculator.down_payment = 100_000
+      calculator.valid?
+      expect(calculator.errors[:down_payment]).to include("can't be greater than or equal to the property price")
+    end
+
+    it 'should add error if down payment is less than 5% of property price' do
       calculator.down_payment = 1000
       calculator.valid?
       expect(calculator.errors[:down_payment]).to include("can't be less than 5% of property price")
